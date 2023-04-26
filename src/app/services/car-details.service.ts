d8ca13fb4bff303165d4fe04455f7c2fd3b288fc
Jigneshvaray Vasava <125241410+jigneshvaray@users.noreply.github.com>
2023-04-26 10:56:06 +0530

    car-details.service.ts file https to http changes

--- src/app/services/car-details.service.ts
+++ src/app/services/car-details.service.ts
@@ -8,7 +8,7 @@ import { ListResponseModel } from '../models/listResponseModel';
   providedIn: 'root'
 })
 export class CarDetailsService {
-  apiUrl="https://localhost:44388/api/";
+  apiUrl="http://localhost:44388/api/";
   constructor(private httpClient:HttpClient) { }
 
   getCarDetail(carId:number):Observable<ListResponseModel<CarDetail>>{
